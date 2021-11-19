27cc57b5ab546c843229801b867e3948a974a477
oisheemaz <oishee.sde@gmail.com>
2021-11-19 20:17:58 +0530

    Added Dynamic content via EJS templates

--- routes/shop.js
+++ routes/shop.js
@@ -2,12 +2,18 @@ const path = require('path')
 
 const express = require('express')
 
-const roothDir = require('../util/path')
+const rootDir = require('../util/path')
+const adminData = require('./admin')
 
 const router = express.Router()
 
 router.get('/', (req, res, next) => {
-    res.sendFile(path.join(roothDir, 'views', 'shop.html'))
+    const products = adminData.products
+    res.render('shop', {
+        prods: products,
+        pageTitle: 'Shop',
+        path: '/',
+    })
 })
 
 module.exports = router
