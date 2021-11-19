64f39238392e8f2edd7a81342a7322acee79876c
oisheemaz <oishee.sde@gmail.com>
2021-11-20 00:26:49 +0530

    added dynamic routes

--- routes/shop.js
+++ routes/shop.js
@@ -10,11 +10,15 @@ router.get('/', shopController.getIndex)
 
 router.get('/products', shopController.getProducts)
 
+router.get('/products/:productId', shopController.getProduct)
+
 router.get('/cart', shopController.getCart)
+
+router.get('/cart-delete-item', shopController.postCartDeleteProduct)
 
 router.get('/orders', shopController.getOrders)
 
-// router.get('/checkout', shopController.getCheckout)
+router.get('/checkout', shopController.getCheckout)
 
 
 
