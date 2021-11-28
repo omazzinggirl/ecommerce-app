86f7c8ee4a27aac271444923332bca15a6d5c818
oisheemaz <oishee.sde@gmail.com>
2021-11-28 19:32:15 +0530

    refactored using Sequelize ORM

--- util/database.js
+++ util/database.js
@@ -1,10 +1,9 @@
-const mysql = require('mysql2')
+const Sequelize = require("sequelize");
 
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'ecommerce-app',
-    password: 'mysql123'
-})
+const sequelize = new Sequelize("ecommerce-app", "root", "mysql123", {
+  dialect: "mysql",
+  host: "localhost",
+});
 
-module.exports = pool.promise();
+
+module.exports = sequelize
